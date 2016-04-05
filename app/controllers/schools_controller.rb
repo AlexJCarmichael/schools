29ee766511cfd655a7a375e1c7f0cff2ffeaa4c0
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-05 17:41:37 -0500

    Full crud for schools

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,56 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = School.order(:name).page params[:page]
+  end
+
+  def show
+    @school = get_school
+  end
+
+  def edit
+    @school = get_school
+  end
+
+  def update
+    @school = get_school
+
+    if @school.update(school_params)
+      redirect_to @school
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+
+    if @school.save
+      redirect_to @school
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @school = get_school
+
+    if @school.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def get_school
+    School.find(params.fetch(:id))
+  end
+
+  def school_params
+    params.require(:school).permit(:name, :mascot, :address)
+  end
+end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,4 @@
 class Teacher < ActiveRecord::Base
   belongs_to :school
+  has_many :students
 end
